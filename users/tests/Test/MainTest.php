4db70d9089b0d8c091bd1cac59c083fc2ba5517e
Vladimir Che <vchernomyrdin@treto.ru>
2019-11-19 00:01:29 +0600

    users. prepare some tests, too raw

--- users/tests/Test/MainTest.php
+++ users/tests/Test/MainTest.php
@@ -6,12 +6,13 @@ use Phalcon\Di;
 use Phalcon\Loader;
 
 error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
-define('ROOT_PATH', realpath(__DIR__ . '../..'));
-define('TESTS_PATH', ROOT_PATH . '/tests');
-defined('APP_PATH') || define('APP_PATH', ROOT_PATH . '/users');
-define('IS_CLI', false);
+//ini_set('display_errors', 1);
+//
+//define('ROOT_PATH', realpath(__DIR__ . '../..'));
+//define('TESTS_PATH', ROOT_PATH . '/tests');
+//defined('APP__PATH') || define('APP__PATH', ROOT_PATH . '/users');
+//define('IS_CLI', false);
+$config = include __DIR__.'/../../app/config/config.php';
 
 //including /var/www/phalcon-start/tests: + EX: include_path=".:/phpfpm/includes"
 set_include_path(TESTS_PATH . PATH_SEPARATOR . get_include_path());
@@ -26,5 +27,5 @@ $loader->registerDirs([TESTS_PATH]);
 $loader->register();
 
 // Add any needed services to the DI here
-$di = require_once APP_PATH . '/bootstrap.phpfpm';
+$di = require_once APP__PATH . '/bootstrap.phpfpm';
 Di::setDefault($di);
